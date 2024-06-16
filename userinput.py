d561de0fa326cdf41145596c64d3f3232b428d64
sathiyamoorthi1810 <57190720+sathiyamoorthi1810@users.noreply.github.com>
2024-06-16 17:20:55 +0530

    python user input()

---
+++ userinput.py
@@ -0,0 +1,65 @@
+# input()
+# print() - built in function
+# sum of numbers(additon)
+# difference of numbers(substraction)
+# product of numbers(multiplication)
+# // - floor division
+# /- division
+# % - module
+# int ((input))
+# float ((input))
+# ()- paranthesis, {} - curly brackets, [] - square bracket
+#type()- view the type
+#print(type(variable name))
+
+'''student_name = input("Enter your name:")
+student_age = int( input("Enter your age:"))
+student_percent = float(input("enter your percentage:"))
+print("Your name is ",student_name)
+print (type(student_name))
+print("Your age is ",student_age)
+print (type(student_age))
+print("Your percentage is ",student_percent)
+print (type(student_percent))
+
+#int
+college_age = int(input("what is your age:"))
+print("your age is:",college_age)
+#float
+college_percentage = float (input("what is your percentage"))
+print("your percentage is",college_percentage)
+
+#.split()
+#adding multiple line in single line using split keyword
+name1,name2,name3 = input("Enter the Name:").split()
+print("Name1:",name1)
+print("Name2:",name2)
+print("Name3:",name3)
+
+#split(',')
+name1,name2,name3 = input("Enter the Name:").split(',')
+print("Name1:",name1)
+print("Name2:",name2)
+print("Name3:",name3)'''
+
+#list([])
+room = [1,2,3,4]
+print("call room no:",(room[1]))
+
+#list using symbols
+room = ["1","2","3","4"]
+print(",".join(room))
+
+
+
+
+# floor division,division, module
+'''a= 11
+b=2
+
+print("floor division value of 11 of 2=",a//b)
+print("division value of 11 of 2=",a/b)
+print("module value of 11 of 2=",a%b)'''
+
+
+
